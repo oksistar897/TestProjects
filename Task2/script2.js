b7a0f37510eeba35c0786277d94d16f8bbcbb352
oksistar897 <90827933+oksistar897@users.noreply.github.com>
2021-09-20 22:59:00 +0300

    Task2

---
+++ Task2/script2.js
@@ -0,0 +1,18 @@
+/*Task number 1*/
+let getMostRepeat = (arr1, arr2) => {
+        let arr = arr1.concat(arr2);
+        let newObj = {}
+        var result = arr.reduce(function(acc, el) {
+            acc[el] = (acc[el] || 0) + 1;
+            return acc;
+        }, newObj);
+        let getResult = Object.entries(newObj).reduce((ac, cur) => ac[1] > cur[1] ? ac : cur)[0];
+        console.log(getResult)
+    }
+
+
+/*Task number 2*/
+let uniqArr = (arr) => {
+    let arrUniq = [...new Set(arr)];
+    console.log(arrUniq);
+}
